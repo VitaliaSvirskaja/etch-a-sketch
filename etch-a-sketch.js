66cd91c3d5f7a19d094fc978c66b73cc1b6114c0
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-02-26 16:33:57 +0100

    Reset feature added

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,23 @@
-for (let i = 1; i <= 256; i++) {
-    const wrapper = document.querySelector(".wrapper");
-    const divElement = document.createElement("div");
-    wrapper.appendChild(divElement)
-    divElement.classList.add("element");
-    divElement.addEventListener("mouseover", function () {
-        divElement.style.backgroundColor = "black";
-        console.log("test")
-    });
+function initializePanes() {
+    for (let i = 1; i <= 256; i++) {
+        const wrapper = document.querySelector(".wrapper");
+        const divElement = document.createElement("div");
+        wrapper.appendChild(divElement)
+        divElement.classList.add("element");
+        divElement.addEventListener("mouseover", function () {
+            divElement.style.backgroundColor = "black";
+            console.log("test")
+        });
+    }
 }
+
+initializePanes();
+
+const reset = document.querySelector(".resetButton");
+reset.addEventListener("click", function reset() {
+    const wrapper = document.querySelector(".wrapper")
+    while (wrapper.firstChild) {
+        wrapper.removeChild(wrapper.firstChild)
+    }
+    initializePanes();
+})
